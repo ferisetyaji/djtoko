a631754f2feee6b84e8a5505f2b2be66cb42b3bb
Feri Setyaji <39894344+ferisetyaji@users.noreply.github.com>
2021-02-09 08:09:24 +0700

    crud user

--- admintoko/urls.py
+++ admintoko/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
-from .views.users import HomeView, UserView
+from .views.users import UserView, addUserView, editUserView
 
 urlpatterns = [
-    path('', HomeView.as_view(template_name='admin/dashboard.html'), name='dashboard'),
+    path('', TemplateView.as_view(template_name='admin/dashboard.html'), name='dashboard'),
     path('user/', UserView.as_view(template_name='admin/user.html'), name='user'),
-    path('user/tambah_user', UserView.as_view(template_name='admin/user_form.html'), name='tambah_user'),
-    path('user/edit_user/<id>', UserView.as_view(template_name='admin/user_form.html'), name='edit_user'),
+    path('user/tambah_user', addUserView.as_view(template_name='admin/user_form.html'), name='tambah_user'),
+    path('user/edit_user/<id>', editUserView.as_view(template_name='admin/user_form.html'), name='edit_user'),
 ]
